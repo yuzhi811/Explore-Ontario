bd98f6d7b742312c9d3b8f88d9e9d1899560cfd6
Ellen Habeck Simon <habe0031@algonquinlive.com>
2022-11-14 13:56:46 -0500

    Created nav burger icon and nav items, styled nav, added titles in every page.

--- assignment_2/scripts/main.js
+++ assignment_2/scripts/main.js
@@ -1,1 +1,7 @@
-js
+function showMenu() {
+  document.getElementById("vertical-menu").classList.add("show");
+}
+
+function hideMenu() {
+  document.getElementById("vertical-menu").classList.remove("show");
+}
